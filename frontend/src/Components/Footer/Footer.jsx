dd3e2b9bc8fbe1106a47b5e4da426dda97c3d9ef
Jafar610 <jafarmohamed955@gmail.com>
2026-06-21 22:19:49 +0300

    initial footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <section>
+         {/* footer */}
+         <div className='bg-[#131921] text-white p-4'>
+          <div className='flex justify-between items-center'>
+            <div>
+              <p>Amazon Clone</p>
+            </div>
+            <div>
+              <p>Contact Us</p>
+            </div>
+          </div>
+         </div>
+      </section>
+    </>
   )
 }
 
