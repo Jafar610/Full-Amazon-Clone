33b9ceba4b6a9e65ad076967220e1055976ec447
Jafar610 <jafarmohamed955@gmail.com>
2026-06-23 23:40:09 +0300

    5th commit

--- frontend/src/Pages/Header.jsx
+++ frontend/src/Pages/Header.jsx
@@ -1,46 +1,43 @@
-import React from 'react'
-
+import React from "react";
+import Location from '@mui/icons-material/LocationOnOutlined';
 function Header() {
   return (
     <>
       <section>
-        <div className='flex bg-[#131921] text-white items-center justify-between p-2'>
+        <div className="flex bg-[#131921] text-white items-center justify-between p-2">
           {/* logo */}
-          <div className=''>
+          <div className="">
             <a href="/">
-              <img src="https://pngimg.com/uploads/amazon/small/amazon_PNG25.png" alt=""  className='w-40'/>
+              <img
+                src="https://pngimg.com/uploads/amazon/small/amazon_PNG25.png"
+                alt=""
+                className="w-40"
+              />
             </a>
           </div>
 
           {/* Location */}
-          <div className='flex items-center gap-2'>
-
-            <div>
-
-            </div>
-
+          <div className="flex items-center gap-2">
+            <Location />
             <div>
               <p>Location</p>
               <p>Ethiopia</p>
             </div>
-
           </div>
 
           {/* search bar */}
 
-          <div className='flex items-center gap-2'>
+          <div className="flex items-center gap-2">
             <select name="" id="">
               <option value="">All</option>
             </select>
             <input type="text" />
             <button>Search</button>
           </div>
-
         </div>
       </section>
-
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
