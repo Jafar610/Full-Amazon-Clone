51ba9120ffd0f30f3575e61911c250218785304a
Jafar610 <jafarmohamed955@gmail.com>
2026-07-06 21:21:48 +0300

    10th commit

--- frontend/src/Pages/CategoryInfo.jsx
+++ frontend/src/Pages/CategoryInfo.jsx
@@ -2,7 +2,7 @@ function CategoryInfo({ name, image, link }) {
   return (
     // category card with image and name of category
 
-    <div className="flex flex-wrap justify-center gap-4 relative top-70 left-0 right-0">
+    <div className="flex flex-wrap justify-center gap-4 relative top-70 left-0 right-0 hover:pointer">
       <div className="w-70 h-70 bg-gray-100 rounded-md flex flex-col items-center justify-center ">
         <p className="text-center mt-2 font-bold text-xl">{name}</p>
         <img src={image} alt={name} className="w-50 h-50 object-contain" />
