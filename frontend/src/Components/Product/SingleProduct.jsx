8e0289c05726b8e156a7e5848ca67f2fb8bb1d85
Jafar610 <jafarmohamed955@gmail.com>
2026-07-07 21:24:47 +0300

    10th commit

---
+++ frontend/src/Components/Product/SingleProduct.jsx
@@ -0,0 +1,14 @@
+function SingleProduct() {
+  return (
+    <section>
+        {/* product cards */}
+        <div>
+            <img src="" alt="" />
+            <h2>Product Name</h2>
+            
+        </div>
+    </section>
+  )
+}
+
+export default SingleProduct
---
+++ frontend/src/Pages/ProductCard.jsx
@@ -0,0 +1,12 @@
+import {Rating} from 'react-simple-star-rating'
+function ProductCard() {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h2>Product Name</h2>
+      <Rating />
+    </div>
+  )
+}
+
+export default ProductCard
